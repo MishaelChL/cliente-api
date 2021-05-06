f3ccac4e75df939f473557dff9a93f060606b2e6
MishaelChL <jose.mishael.chile@gmail.com>
2021-05-06 18:58:37 -0400

    creando la navegacion

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, {Fragment} from 'react';
 
 /* Layout */
 import Header from "./components/layout/Header";
+import Navegacion from "./components/layout/Navegacion";
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <div class="grid contenedor contenido-principal">
+        <Navegacion/>
+        <main class="caja-contenido col-9">
+          {/* TODO : A los diferentes componentes */}
+        </main>
+      </div>
     </Fragment>
   );
 }
