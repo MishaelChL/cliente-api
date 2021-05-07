07408f8429e147fda1d342f910cdbd7e6df94840
MishaelChL <jose.mishael.chile@gmail.com>
2021-05-07 10:16:44 -0400

    habilitar cors en el back y consumir api

--- src/components/clientes/Clientes.js
+++ src/components/clientes/Clientes.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+import clienteAxios from "../../config/axios";
 
 function Clientes() {
+
+    //use effect es para los hooks, es similar a componentdidmount y willmount
+    useEffect( () => {
+
+        //query a la api
+        const consultarApi = async () => {
+            const clientesConsulta = await clienteAxios.get("/clientes");
+            console.log(clientesConsulta);
+        }
+
+        consultarApi();
+    },[]);
+
     return(
         <h2>Clientes</h2>
     );
