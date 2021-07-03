e3672c8e614b1944f54945ae27224056a32eed6a
MishaelChL <jose.mishael.chile@gmail.com>
2021-07-03 10:10:17 -0400

    Editar Producto - Obteniendo el producto a editar

--- src/components/productos/EditarProducto.js
+++ src/components/productos/EditarProducto.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Swal from 'sweetalert2';
+import clienteAxios from '../../config/axios';
+import {withRouter} from 'react-router-dom';
+import Spinner from '../layout/Spinner';
 
-function EditarProducto() {
+function EditarProducto(props) {
+
+    //obtener el id del producto
+    const {id} = props.match.params;
+
+    //producto = state, y funcion  para actualizar
+    const [producto, guardarProducto] = useState({
+        nombre: '',
+        precio: '',
+        imagen: ''
+    })
+
+    useEffect(() => {
+        //consultar la api para traer el producto a editar
+        const consultarAPI = async () => {
+            const productoConsulta = await clienteAxios.get(`/productos/${id}`);
+            console.log(productoConsulta.data);
+            guardarProducto(productoConsulta.data);
+        }
+        consultarAPI();
+    }, []);
+
     return(
-        <h2>Productos</h2>
+        <h2>EditarProductos</h2>
     );
 }
 
