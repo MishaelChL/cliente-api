1b212a6ea1d7ba0ecd25ee894a80ab17d9e2095b
MishaelChL <jose.mishael.chile@gmail.com>
2021-07-03 10:55:30 -0400

    Editar Producto - Mostrando la informacion del producto a editar

--- src/components/productos/EditarProducto.js
+++ src/components/productos/EditarProducto.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
 import Swal from 'sweetalert2';
 import clienteAxios from '../../config/axios';
 import {withRouter} from 'react-router-dom';
@@ -16,6 +16,9 @@ function EditarProducto(props) {
         imagen: ''
     })
 
+    //archivo = state, guardarArchivo = setState
+    const [archivo, guardarArchivo] = useState('');
+
     useEffect(() => {
         //consultar la api para traer el producto a editar
         const consultarAPI = async () => {
@@ -26,8 +29,57 @@ function EditarProducto(props) {
         consultarAPI();
     }, []);
 
+    //leer los datos del formulario
+    const leerInformacionProducto = e => {
+        guardarProducto({
+            //obtener una copia del state y agregar el nuevo
+            ...producto,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //coloca la imagen en el state 
+    const leerArchivo = e => {
+        guardarArchivo(e.target.files[0]);
+    }
+
+    //extraer los valores del state
+    const {nombre, precio, imagen} = producto;
+
+    if(!nombre) return <Spinner />
+
     return(
-        <h2>EditarProductos</h2>
+        <Fragment>
+            <h2>Editar Producto</h2>
+
+            <form>
+                <legend>Llena todos los campos</legend>
+
+                <div className="campo">
+                    <label>Nombre:</label>
+                    <input type="text" placeholder="Nombre Producto" name="nombre" onChange={leerInformacionProducto} defaultValue={nombre} />
+                </div>
+
+                <div className="campo">
+                    <label>Precio:</label>
+                    <input type="number" name="precio" min="0.00" step="0.01" placeholder="Precio" onChange={leerInformacionProducto} defaultValue={precio} />
+                </div>
+
+                <div className="campo">
+                    <label>Imagen:</label>
+                    {
+                        imagen ? (
+                            <img src={`http://localhost:5000/${imagen}`} alt="imagen" width="300" />  
+                        ) : null
+                    }
+                    <input type="file"  name="imagen" onChange={leerArchivo}  />
+                </div>
+
+                <div className="enviar">
+                        <input type="submit" className="btn btn-azul" value="Agregar Producto" />
+                </div>
+            </form>
+        </Fragment> 
     );
 }
 
