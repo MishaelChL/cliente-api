3615958ccd7c7b049f7f17d84449da950bdc6b97
MishaelChL <jose.mishael.chile@gmail.com>
2021-07-02 22:09:12 -0400

    creando formulario para los nuevos productos

--- src/components/productos/NuevoProducto.js
+++ src/components/productos/NuevoProducto.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, {useState, Fragment} from 'react';
 
 function NuevoProducto() {
+    
+    //producto = state, guardarProducto = setState
+    const [producto, guardarProducto] = useState({
+        nombre: '',
+        precio: ''
+    });
+
+    //archivo = state, guardarArchivo = setState
+    const [archivo, guardarProducto] = useState('');
+
     return(
-        <h2>Productos</h2>
+        <Fragment>
+            <h2>Nuevo Producto</h2>
+
+            <form>
+                <legend>Llena todos los campos</legend>
+
+                <div className="campo">
+                    <label>Nombre:</label>
+                    <input type="text" placeholder="Nombre Producto" name="nombre" />
+                </div>
+
+                <div className="campo">
+                    <label>Precio:</label>
+                    <input type="number" name="precio" min="0.00" step="0.01" placeholder="Precio" />
+                </div>
+
+                <div className="campo">
+                    <label>Imagen:</label>
+                    <input type="file"  name="imagen" />
+                </div>
+
+                <div className="enviar">
+                        <input type="submit" className="btn btn-azul" value="Agregar Producto" />
+                </div>
+            </form>
+        </Fragment> 
     );
 }
 
