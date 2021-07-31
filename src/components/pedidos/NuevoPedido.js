83412fc0546558734368878e4397892b323072c7
MishaelChL <jose.mishael.chile@gmail.com>
2021-07-30 20:47:46 -0400

    agregando contenido a nuevos pedidos

--- src/components/pedidos/NuevoPedido.js
+++ src/components/pedidos/NuevoPedido.js
@@ -1,8 +1,108 @@
-import React from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
+import clienteAxios from '../../config/axios';
 
-function NuevoPedido(){
+function NuevoPedido(props){
+
+    //extraer id del cliente para poder hacer la consulta a la bd, para saber que cliente es...
+    const { id } = props.match.params;  //1
+
+    //state
+    const [cliente, guardarCliente] = useState({}); //3
+
+    useEffect(() => {       //Ojooooooooooooooooooo primero se hace el useeffect, luego el usestate
+        //obtener el cliente
+        const consultarAPI = async () => {
+            //consultar el cliente actual 
+            const resultado = await clienteAxios.get(`/clientes/${id}`); //2
+            // console.log(resultado.data);
+            guardarCliente(resultado.data);
+        }
+
+        //llamar a la API
+        consultarAPI();
+
+    }, []);
+
     return(
-        <h2>Nuevo Pedido</h2>
+        <Fragment>
+            <h2>Nuevo Pedido</h2>
+
+            <div className="ficha-cliente">
+                <h3>Datos de Cliente</h3>
+                <p>Nombre: {cliente.nombre} {cliente.apellido}</p>
+                <p>Telefono: {cliente.telefono}</p>
+            </div>
+
+            <form>
+                <legend>Busca un Producto y agrega una cantidad</legend>
+
+                <div className="campo">
+                    <label>Productos:</label>
+                    <input type="text" placeholder="Nombre Productos" name="productos" />
+                </div>
+
+                <ul className="resumen">
+                    <li>
+                        <div className="texto-producto">
+                            <p className="nombre">Macbook Pro</p>
+                            <p className="precio">$250</p>
+                        </div>
+                        <div className="acciones">
+                            <div className="contenedor-cantidad">
+                                <i className="fas fa-minus"></i>
+                                <input type="text" name="cantidad" />
+                                <i className="fas fa-plus"></i>
+                            </div>
+                            <button type="button" className="btn btn-rojo">
+                                <i className="fas fa-minus-circle"></i>
+                                    Eliminar Producto
+                            </button>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="texto-producto">
+                            <p className="nombre">Macbook Pro</p>
+                            <p className="precio">$250</p>
+                        </div>
+                        <div className="acciones">
+                            <div className="contenedor-cantidad">
+                                <i className="fas fa-minus"></i>
+                                <input type="text" name="cantidad" />
+                                <i className="fas fa-plus"></i>
+                            </div>
+                            <button type="button" className="btn btn-rojo">
+                                <i className="fas fa-minus-circle"></i>
+                                    Eliminar Producto
+                            </button>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="texto-producto">
+                            <p className="nombre">Macbook Pro</p>
+                            <p className="precio">$250</p>
+                        </div>
+                        <div className="acciones">
+                            <div className="contenedor-cantidad">
+                                <i className="fas fa-minus"></i>
+                                <input type="text" name="cantidad" />
+                                <i className="fas fa-plus"></i>
+                            </div>
+                            <button type="button" className="btn btn-rojo">
+                                <i className="fas fa-minus-circle"></i>
+                                    Eliminar Producto
+                            </button>
+                        </div>
+                    </li>
+                </ul>
+                <div className="campo">
+                    <label>Total:</label>
+                    <input type="number" name="precio" placeholder="Precio" readonly="readonly" />
+                </div>
+                <div className="enviar">
+                    <input type="submit" className="btn btn-azul" value="Agregar Pedido" />
+                </div>
+            </form>
+        </Fragment>
     )
 }
 
