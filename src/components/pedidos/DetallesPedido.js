4ade55a08ab929ceaeaa9a4bcf31f23ed66af4fe
MishaelChL <jose.mishael.chile@gmail.com>
2021-08-01 21:30:09 -0400

    listando los pedidos

--- src/components/pedidos/DetallesPedido.js
+++ src/components/pedidos/DetallesPedido.js
@@ -1,32 +1,25 @@
 import React from 'react';
 function DetallesPedido(props){
+    const {cliente} = props.pedido;
     return(
         <li className="pedido">
             <div className="info-pedido">
-                <p className="id">ID: 0192019201291201</p>
-                <p className="nombre">Cliente: Juan Pablo De la torre</p>
+                <p className="id">ID: {props.pedido._id}</p>
+                <p className="nombre">Cliente: {cliente.nombre} {cliente.apellido}</p>
 
                 <div className="articulos-pedido">
                     <p className="productos">Artículos Pedido: </p>
                     <ul>
-                        <li>
-                            <p>Macbook Pro</p>
-                            <p>Precio: $3000</p>
-                            <p>Cantidad: 4</p>
-                        </li>
-                        <li>
-                            <p>Macbook Pro</p>
-                            <p>Precio: $3000</p>
-                            <p>Cantidad: 4</p>
-                        </li>
-                        <li>
-                            <p>Macbook Pro</p>
-                            <p>Precio: $3000</p>
-                            <p>Cantidad: 4</p>
-                        </li>
+                        {props.pedido.pedido.map(articulos => (
+                            <li key={props.pedido._id+articulos.producto._id}>
+                                <p>Nombre: {articulos.producto.nombre}</p>
+                                <p>Precio: ${articulos.producto.precio}</p>
+                                <p>Cantidad: {articulos.cantidad}</p>
+                            </li>
+                        ))}
                     </ul>
                 </div>
-                <p className="total">Total: $3,500 </p>
+                <p className="total">Total: ${props.pedido.total} </p>
             </div>
             <div className="acciones">
                 <button type="button" className="btn btn-rojo btn-eliminar">
