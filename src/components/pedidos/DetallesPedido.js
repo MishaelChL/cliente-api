4433a437644c836e6b81bec59b2c6c38b3ee2f92
MishaelChL <jose.mishael.chile@gmail.com>
2021-08-01 21:41:42 -0400

    eliminando pedidos

--- src/components/pedidos/DetallesPedido.js
+++ src/components/pedidos/DetallesPedido.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import clienteAxios from '../../config/axios';
+import Swal from 'sweetalert2';
+
 function DetallesPedido(props){
     const {cliente} = props.pedido;
+    
+    const eliminarPedido = id => {
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: "Luego no hay vuelta atrás",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, elimínalo!'          
+        }).then((result) => {
+            if (result.value) {                 
+                // llamado a axios
+                clienteAxios.delete(`/pedidos/${id}`).then(res => {
+                    Swal.fire('Eliminado!',
+                    'El cliente se ha eliminado.',
+                    'success'
+                )})
+            }
+        }); 
+    }   
+
     return(
         <li className="pedido">
             <div className="info-pedido">
@@ -22,7 +47,7 @@ function DetallesPedido(props){
                 <p className="total">Total: ${props.pedido.total} </p>
             </div>
             <div className="acciones">
-                <button type="button" className="btn btn-rojo btn-eliminar">
+                <button type="button" className="btn btn-rojo btn-eliminar" onClick={() => eliminarPedido(props.pedido._id)}>
                     <i className="fas fa-times"></i>
                     Eliminar Pedido
                 </button>
