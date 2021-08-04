682badb0310d95818c4d9afeef251c33da692ce7
MishaelChL <jose.mishael.chile@gmail.com>
2021-08-04 19:12:16 -0400

    asegurando un componente

--- src/components/clientes/Clientes.js
+++ src/components/clientes/Clientes.js
@@ -3,14 +3,14 @@ import React, {useEffect, useState, Fragment, useContext} from 'react';
 import clienteAxios from "../../config/axios";
 
 import Cliente from "./Cliente";
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 
 import Spinner from '../layout/Spinner';
 
 //importar el context
 import { CRMContext } from '../context/CRMContext';
 
-function Clientes() {
+function Clientes(props) {
 
     //trabajar con el state
     //clientes = statem guardarClientes = funcion para guardar el state
@@ -18,33 +18,49 @@ function Clientes() {
 
     //utilizar valores del context
     const [auth, guardarAuth] = useContext(CRMContext);
-    console.log(auth);
+    // console.log(auth);
 
     //use effect es para los hooks, es similar a componentdidmount y willmount
     useEffect( () => {
-        const abortController = new AbortController();
-        const signal = abortController.signal;
-        
-        //query a la api
-        async function consultarApi(){
-            const clientesConsulta = await clienteAxios.get("/clientes", { 
-                signal, 
-                headers: {
-                    Authorization: `Bearer HOLAMUNDO`
+        if(auth.token !== ''){
+            const abortController = new AbortController();
+            const signal = abortController.signal;
+            
+            //query a la api
+            async function consultarApi(){
+                try {
+                    const clientesConsulta = await clienteAxios.get("/clientes", { 
+                        signal, 
+                        headers: {
+                            Authorization: `Bearer ${auth.token}`
+                        }
+                    });
+                    // console.log(clientesConsulta.data);
+    
+                    //colocar el resultado en el state
+                    guardarClientes(clientesConsulta.data);
+                } catch (error) {
+                    //error con autorizacion
+                    if(error.response.status == 500){
+                        props.history.push('/iniciar-sesion');
+                    }
                 }
-            });
-            // console.log(clientesConsulta.data);
+            }
 
-            //colocar el resultado en el state
-            guardarClientes(clientesConsulta.data);
+            consultarApi();
+
+            return function cleanup() {
+                abortController.abort();
+            };
+        }else{
+            props.history.push('/iniciar-sesion');
         }
-
-        consultarApi();
-
-        return function cleanup() {
-            abortController.abort();
-        };
     },[clientes]); //los corchetes, cuando los clientes cambien, vuelve a hacer un llamado a consultar Api, es como un refresh a la misma pagina
+
+    //Si el state está como false
+    if(!auth.auth){
+        props.history.push('/iniciar-sesion');
+    }
 
     if(!clientes.length) return <Spinner />
 
@@ -70,4 +86,4 @@ function Clientes() {
     );
 }
 
-export default Clientes;
+export default withRouter(Clientes);
