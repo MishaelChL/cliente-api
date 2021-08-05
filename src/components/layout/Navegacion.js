bdff4a61de30590859e5aed8aed57b6c5c36f15a
MishaelChL <jose.mishael.chile@gmail.com>
2021-08-04 20:56:14 -0400

    cerrar sesion y ocultar partes de la interfaz si el usuario no está autenticado

--- src/components/layout/Navegacion.js
+++ src/components/layout/Navegacion.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Link } from 'react-router-dom';
+import { CRMContext } from '../context/CRMContext';
 
 const Navegacion = () => { //Esta es una forma de crear funciones, hay otra con functions
+    
+    const [auth, guardarAuth] = useContext(CRMContext);
+
+    if(!auth.auth) return null;
+    
     return (
         <aside className="sidebar col-3">
             <h2>Administración</h2>
 
-            <nav className="navegacion">
-                <Link to={"/"} className="clientes">Clientes</Link>
-                <Link to={"/productos"} className="productos">Productos</Link>
-                <Link to={"/pedidos"} className="pedidos">Pedidos</Link>
-            </nav>
+            {
+                auth.auth ? (
+                    <nav className="navegacion">
+                        <Link to={"/"} className="clientes">Clientes</Link>
+                        <Link to={"/productos"} className="productos">Productos</Link>
+                        <Link to={"/pedidos"} className="pedidos">Pedidos</Link>
+                    </nav>
+                ):null
+            }
+
+            
         </aside>
     );
 }
